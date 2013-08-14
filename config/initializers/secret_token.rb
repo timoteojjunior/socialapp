8b669d39a12dd9cf43c5996aea51e7355f581b43
Timoteo Junior <timoteojjunior@gmail.com>
2013-08-14 12:13:58 +0200

    Created Social App And Added Status Crud

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialapp::Application.config.secret_token = '58e404586cede560b43a100cbdae7628471807bd5455d7d75d3045e8cf2b8df8fc8ae797fe312d0975a2ad7e4b8f451e716740bd7b6517df536aed2eb6a806c8'
